e10d93b33e20323e9ecbc18468dd8d44922d0d43
Dima <karyakin.dmitry@gmail.com>
2025-08-29 01:52:51 +0200

    added dependency to an internal expenses service

---
+++ backend/internal/api/expense/dependencies.go
@@ -0,0 +1,13 @@
+package expense
+
+import (
+	"context"
+
+	"github.com/dKariakin/purser/internal/domain/model"
+)
+
+type Service interface {
+	CreateExpense(context.Context, model.Expense) (model.Expense, error)
+}
+
+//go:generate mockgen -destination=mocks/service.go -package=mocks . Service
