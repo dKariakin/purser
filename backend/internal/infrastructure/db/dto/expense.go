10f4085e6f3348808a6b5041ea6de0f7b016d69a
Dima <karyakin.dmitry@gmail.com>
2025-10-31 00:47:58 +0100

    Add DB dto

---
+++ backend/internal/infrastructure/db/dto/expense.go
@@ -0,0 +1,33 @@
+package dto
+
+import (
+	"encoding/json"
+	"io"
+	"strings"
+
+	"github.com/dKariakin/purser/internal/domain/model"
+)
+
+type Expense struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Price     int `json:"price"`
+	CreatedAt int64 `json:"createdAt"`
+}
+
+func FromDomain(e model.Expense) Expense {
+	return Expense {
+		Id: e.Id,
+		Name: e.Name,
+		Price: e.Price,
+	}
+}
+
+func (e Expense) Reader() io.Reader {
+	j, err := json.Marshal(e)
+	if err != nil {
+		return nil
+	}
+
+	return strings.NewReader(string(j))
+}
---
+++ backend/internal/infrastructure/db/dto/expense_test.go
@@ -0,0 +1,51 @@
+package dto
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/dKariakin/purser/internal/domain/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExpenseDto(t *testing.T) {
+	t.Parallel()
+
+	t.Run("When a new DB expense object is created from a domain model, createdAt should not be set", func(t *testing.T) {
+		t.Parallel()
+
+		source := model.Expense{
+			Id: "1",
+			Name: "test",
+			Price: 42,
+		}
+		expected := Expense{
+			Id: "1",
+			Name: "test",
+			Price: 42,
+		}
+
+		actual := FromDomain(source)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("When an expense DTO is converted to reader, a correct object should be returned", func(t *testing.T) {
+		t.Parallel()
+
+		now := time.Now().Unix()
+		source := Expense{
+			Id: "42",
+			Name: " test expense",
+			Price: 42,
+			CreatedAt: now,
+		}
+		j := fmt.Sprintf("{\"id\":\"42\",\"name\":\" test expense\",\"price\":42,\"createdAt\":%v}", now)
+		expected := strings.NewReader(j)
+		result := source.Reader()
+
+		assert.Equal(t, expected, result)
+	})
+}
